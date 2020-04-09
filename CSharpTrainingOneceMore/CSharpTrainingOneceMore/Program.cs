066711a4651ce3e434dc811bb521440fc638f319
mwyrodek <wyrodek.maciej@gmail.com>
2020-04-09 16:29:59 +0200

    example comint and push

--- CSharpTrainingOneceMore/CSharpTrainingOneceMore/Program.cs
+++ CSharpTrainingOneceMore/CSharpTrainingOneceMore/Program.cs
@@ -10,6 +10,8 @@ namespace CSharpTrainingOneceMore
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
         }
     }
 }
